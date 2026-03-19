ab0248841f8e21335565ad869ffa847de4df5b24
saorsak <saorsak@gymcheb.cz>
2026-03-19 13:25:20 +0100

    Update main.py

--- Lekce_02_Input/main.py
+++ Lekce_02_Input/main.py
@@ -1,16 +1,24 @@
-# ==========================================
-# ÚLOHA 1: Registrace (jméno a město)
-# ==========================================
-# Sem napiš svůj kód:
+1. Úloha: Registrace uživatele
+jmeno = input("Jak se jmenuješ? ")
+barva = input("Jaká je tvoje oblíbená barva? ")
+
+print(f"Ahoj {jmeno}, tvoje oblíbená barva je {barva}. To je skvělá volba!")
+
+print() 
 
 
-# ==========================================
-# ÚLOHA 2: Jednoduchá kalkulačka (součet)
-# ==========================================
-# Sem napiš svůj kód:
+2. Úloha: Jednoduchá kalkulačka
+cislo1 = int(input("Zadej první číslo: "))
+cislo2 = int(input("Zadej druhé číslo: "))
+
+soucet = cislo1 + cislo2
+print(f"Součet je: {soucet}")
+
+print() 
 
 
-# ==========================================
-# ÚLOHA 3: Výpočet psího věku (věk * 7)
-# ==========================================
-# Sem napiš svůj kód:
+3. Úloha: Výpočet věku psa
+vek = int(input("Kolik ti je let? "))
+
+psi_vek = vek * 7
+print(f"V psích letech by ti bylo: {psi_vek} let. Haf!")
